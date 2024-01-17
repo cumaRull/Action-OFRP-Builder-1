50a1e1443ab2022c840020bc785f2ec23f1d3d7e
Cuma.Rull <102359107+cumaRull@users.noreply.github.com>
2024-01-17 11:42:31 +0700

    even

--- scripts/ofrpcode.sh
+++ scripts/ofrpcode.sh
@@ -1,56 +1,55 @@
 #!/bin/bash
 
 
-export FOX_VERSION='R12.1'
-export OF_MAINTAINER=ismasrull
-export OF_USE_TWRP_SAR_DETECT='1'
+
 export FOX_VARIANT="RUI4"
-export FOX_BUILD_TYPE="Stable"
- 
-# Binaries & Tools
- export FOX_USE_BASH_SHELL=1
- export FOX_USE_NANO_EDITOR=1
- export FOX_USE_TAR_BINARY=1
- export FOX_USE_SED_BINARY=1
- export FOX_USE_XZ_UTILS=1
- export FOX_ASH_IS_BASH=1
+export TW_DEFAULT_LANGUAGE="en"
+	export LC_ALL="C"
+	export ALLOW_MISSING_DEPENDENCIES=true
 
- # OrangeFox Addons
- export FOX_ENABLE_APP_MANAGER=1
- export FOX_DELETE_AROMAFM=1
+	export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/platform/bootdevice/by-name/recovery"
+	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
+	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
+	export FOX_RECOVERY_BOOT_PARTITION="/dev/block/platform/bootdevice/by-name/boot"
 
- # MIUI & Custom ROMs
- export OF_NO_MIUI_PATCH_WARNING=1
- export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
- export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
- export OF_DEFAULT_KEYMASTER_VERSION=4.1
+	# Alternate Codenames
+	export TARGET_DEVICE_ALT="RMX3191,rmx3191"
 
- # A/B partitioning
- export FOX_VIRTUAL_AB_DEVICE=1
- export OF_AB_DEVICE_WITH_RECOVERY_PARTITION=1
- export OF_ENABLE_LPTOOLS=1
+	export OF_USE_GREEN_LED=0
+	export OF_HIDE_NOTCH=1
+	export OF_USE_MAGISKBOOT=1
+	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+	export OF_NO_MIUI_PATCH_WARNING=1
+	export FOX_USE_BASH_SHELL=1
+	export FOX_ASH_IS_BASH=1
+	export FOX_USE_TAR_BINARY=1
+	export FOX_USE_SED_BINARY=1
+	export FOX_USE_XZ_UTILS=1
+	export OF_QUICK_BACKUP_LIST="/boot;/data;"
+	export OF_PATCH_AVB20=1
+	export FOX_DELETE_AROMAFM=1
+	export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"; # Tuesday, January 1, 2019 12:00:00 AM GMT+00:00
+	export FOX_ENABLE_APP_MANAGER=1
 
- # Store settings at /data/recovery instead of internal storage
- export FOX_USE_DATA_RECOVERY_FOR_SETTINGS=1
+	# R11.1 Settings
+	export FOX_VERSION="R12.1"
+	export FOX_BUILD_TYPE="Stable"
+	export OF_MAINTAINER="ismasrull"
+	
 
- # Flashlight & LEDs
- export OF_FL_PATH1=/system/etc/flashlight
- export OF_USE_GREEN_LED=0
+	# OTA
+	export OF_KEEP_DM_VERITY=1
+	export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
+	export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+	export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
 
- # Remove the loop block errors after flashing ZIPs (Workaround)
- export OF_LOOP_DEVICE_ERRORS_TO_LOG=1
-
- # Use LZ4 ramdisk compression as specified in BoardConfig.mk
- export OF_USE_LZ4_COMPRESSION=true
-
- # Security (Disables MTP & ADB during password prompt)
- export OF_ADVANCED_SECURITY=1
-
-# OTA
-export OF_KEEP_DM_VERITY=1
-export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
-export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
-export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
-        
-export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
-export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
+	# Screen Settings
+	export OF_SCREEN_H=2400
+	export OF_STATUS_H=80
+	export OF_STATUS_INDENT_LEFT=48
+	export OF_STATUS_INDENT_RIGHT=48
+	export OF_CLOCK_POS=1
+	export OF_ALLOW_DISABLE_NAVBAR=0
