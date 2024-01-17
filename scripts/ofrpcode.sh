a652d0513a281c5393f7137d03887720a9a8766b
Cuma.Rull <102359107+cumaRull@users.noreply.github.com>
2024-01-17 09:56:52 +0700

    ofrp code

---
+++ scripts/ofrpcode.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+
+export FOX_VERSION='R12.1'
+export OF_MAINTAINER=ismasrull
+export OF_USE_TWRP_SAR_DETECT='1'
+export FOX_VARIANT="RUI4"
+
+ 
+# Binaries & Tools
+ export FOX_USE_BASH_SHELL=1
+ export FOX_USE_NANO_EDITOR=1
+ export FOX_USE_TAR_BINARY=1
+ export FOX_USE_SED_BINARY=1
+ export FOX_USE_XZ_UTILS=1
+ export FOX_ASH_IS_BASH=1
+
+ # OrangeFox Addons
+ export FOX_ENABLE_APP_MANAGER=1
+ export FOX_DELETE_AROMAFM=1
+
+ # MIUI & Custom ROMs
+ export OF_NO_MIUI_PATCH_WARNING=1
+ export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+ export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+ export OF_DEFAULT_KEYMASTER_VERSION=4.1
+
+ # A/B partitioning
+ export FOX_VIRTUAL_AB_DEVICE=1
+ export OF_AB_DEVICE_WITH_RECOVERY_PARTITION=1
+ export OF_ENABLE_LPTOOLS=1
+
+ # Store settings at /data/recovery instead of internal storage
+ export FOX_USE_DATA_RECOVERY_FOR_SETTINGS=1
+
+ # Flashlight & LEDs
+ export OF_FL_PATH1=/system/etc/flashlight
+ export OF_USE_GREEN_LED=0
+
+ # Remove the loop block errors after flashing ZIPs (Workaround)
+ export OF_LOOP_DEVICE_ERRORS_TO_LOG=1
+
+ # Use LZ4 ramdisk compression as specified in BoardConfig.mk
+ export OF_USE_LZ4_COMPRESSION=true
+
+ # Security (Disables MTP & ADB during password prompt)
+ export OF_ADVANCED_SECURITY=1
+
+# OTA
+export OF_KEEP_DM_VERITY=1
+export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
+export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
+        
+export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
+export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
